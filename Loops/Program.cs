0c922287179dd2a07f12fb2eba954ddcf51ba508
Owen Norman <75017974+OwenNorman1@users.noreply.github.com>
2024-01-04 15:25:12 +0000

    Add files via upload

---
+++ Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double count = 0;
+            do
+            {
+                sum = sum + Math.Pow(count, 2);
+                Console.WriteLine("count: {0}, sum: {1}", count, sum);
+                count++;
+            } while (count != 10);
+                Console.ReadLine();
+            for (int i = 0; i<100; i++)
+            {
+                if (i%2  == 0)
+                {
+                    continue;
+                }
+                Console.Write("{0}, ", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int x = 3; 
+            int y = 5;
+            int sum = 0;
+            sum = calc.add(x, y);
+            Console.WriteLine("Sum: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
