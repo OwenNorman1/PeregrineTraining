686ea4cc1474243a354677dce89a212de7692a09
Owen Norman <75017974+OwenNorman1@users.noreply.github.com>
2024-01-04 15:24:15 +0000

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int x = 0;
+            float f = 0.0f;
+            decimal d = 0.0M;
+            string name = "Joe";
+            string[] names = { "Joe", "Bill", "Bob" };
+            char c = 'a';
+            Boolean b = false;
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FunctionBlock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FunctionBlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 4;
+            int sum = 0;
+            int squared = 0;
+            sum = Add(a, b);
+            squared = Square(a);
+            Console.WriteLine("Sum: {0}", sum);
+            Console.WriteLine("Square: {0}", squared);
+            Console.ReadLine();
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Square(int a)
+        {
+            return a * a;
+        }
+    }
+}
