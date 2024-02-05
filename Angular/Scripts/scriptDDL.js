de9741743a65b8b26499475613bcbbb358ae3248
Owen Norman <75017974+OwenNorman1@users.noreply.github.com>
2024-02-05 09:36:58 +0000

    Add files via upload

---
+++ Angular/Scripts/scriptDDL.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('MyApp', []);
+app.controller('MyController', function ($scope) {
+    $scope.MyVal = "Welcome to Angular.js";
+    $scope.MyFunc = function () {
+        alert('Hello World: ');
+    }
+    $scope.MyFunc2 = function (val) {
+        alert('parameter passed: ' + val);
+    }
+});
